5ac07f1250a8866ac0348bebb5c4dfd22c3078b6
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-10-11 13:13:15 -0500

    i think finished

--- calculator_test.rb
+++ calculator_test.rb
@@ -11,7 +11,18 @@ class TestCalculator < Minitest::Test
   def test_add
     assert_equal 4, @calc.add(2,2)
   end
+
+  def test_subtract
+    assert_equal 5, @calc.subtract(9, 4)
+  end
+
+  def test_multiply
+    assert_equal 10, @calc.multiply(5, 2)
+  end
   
+  def test_divide
+    assert_equal 4, @calc.divide(36, 9)
+  end
   # Write test methods for subtract, multiply, and divide
   
   
